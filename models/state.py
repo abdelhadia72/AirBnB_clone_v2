c39d92b7563c0f874450efcc65aa722a6e5c8e92
FahdAzouz <fahdstudies00@gmail.com>
2023-12-17 20:47:46 +0100

    test

--- models/state.py
+++ models/state.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+"""This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-import shlex
-from models.city import City
+from sqlalchemy import Column, Integer, String
 import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan, delete")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
     @property
     def cities(self):
         var = models.storage.all()
