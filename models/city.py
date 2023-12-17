38b8e4c91a5496cba0f310d0b6f0a519e5de7015
FahdAzouz <fahdstudies00@gmail.com>
2023-12-17 18:27:05 +0100

    update

--- models/city.py
+++ models/city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
--- models/state.py
+++ models/state.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+import shlex
+from models.city import City
+import models
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan, delete")
     @property
     def cities(self):
-        '''getter attribute cities that returns the list of City instances'''
-        from models import storage
-        cities = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
