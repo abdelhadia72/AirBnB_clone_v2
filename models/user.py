c9fdae25654ecf1374410eae1d019779b7b6bc57
FahdAzouz <fahdstudies00@gmail.com>
2023-12-17 21:25:11 +0100

    Add attributes to User class

--- models/user.py
+++ models/user.py
@@ -3,9 +3,16 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user by various attributes
+    Attributes:
+        email: email address
+        password: password to login
+        first_name: first name of user
+        last_name: last name of user"""
     __tablename__ = 'users'
     
     email = Column(String(128), nullable=False)
