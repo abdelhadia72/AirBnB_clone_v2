b142b4a2b8fa7d6e5a2a8337999fff23bb109541
FahdAzouz <fahdstudies00@gmail.com>
2023-12-18 17:55:09 +0100

    update

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlachehmy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
