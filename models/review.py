227e2934dc0b24b2719c0fea10566a149c299882
abdelhadia72 <thisisavnb@gmail.com>
2023-12-18 16:22:45 +0100

    Fixing

--- models/review.py
+++ models/review.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Review(Base, BaseModel):
+class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
-    id = Column(Integer, primary_key=True,  nullable=False)
-    content = Column(String(60),  nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'),  nullable=False)
+    place_id = ""
+    user_id = ""
+    text = ""
