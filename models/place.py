7a5b1e24110c3d18fb5c1815d58263e79695444b
FahdAzouz <fahdstudies00@gmail.com>
2023-12-19 12:44:35 +0100

    update

--- models/place.py
+++ models/place.py
@@ -8,6 +8,18 @@ from os import getenv
 import models
 from models.review import Review
 
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
+
+
 class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
@@ -38,6 +50,10 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                            viewonly=False,
+                            back_populates="place_amenities")
+    
     else:
         @property
         def reviews(self):
@@ -47,3 +63,15 @@ class Place(BaseModel, Base):
                 if key.place_id == self.id:
                     lista.append(key)
             return lista
+        
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
+
