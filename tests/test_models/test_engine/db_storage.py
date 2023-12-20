6640197d45f0d58c31febb24177ae56a2f8dde24
abdelhadia72 <thisisavnb@gmail.com>
2023-12-21 00:51:50 +0100

    testing

--- tests/test_models/test_engine/db_storage.py
+++ tests/test_models/test_engine/db_storage.py
@@ -61,3 +61,21 @@ class DBStorage:
         """
         self.__session.commit()
 
+    def delete(self, obj=None):
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """configuration
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.close()
