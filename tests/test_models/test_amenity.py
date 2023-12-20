8384eba555e613bf10a59b73dfd19cceab80ea6c
abdelhadia72 <thisisavnb@gmail.com>
2023-12-21 00:20:15 +0100

    testing 1

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-""" """
+""" testing the amenity """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from datetime import datetime
+from unittest.mock import patch
+from time import sleep
+from os import getenv
+import pycodestyle
+import inspect
+import unittest
 
-
-class test_Amenity(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
-
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
