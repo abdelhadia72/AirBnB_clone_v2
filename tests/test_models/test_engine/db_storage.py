2b9b1146ed429a97e642c356daa006534cb92fc2
abdelhadia72 <thisisavnb@gmail.com>
2023-12-21 00:51:41 +0100

    testing

---
+++ tests/test_models/test_engine/db_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""test db module"""
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+class DBStorage:
+    """create environmental"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """"""
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for ele in query:
+                key = "{}.{}".format(type(ele).__name__, ele.id)
+                dic[key] = ele
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for ele in query:
+                    key = "{}.{}".format(type(ele).__name__, ele.id)
+                    dic[key] = ele
+        return (dic)
+
