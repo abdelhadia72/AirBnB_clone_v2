44c46adc1514bcbefcd961815772dd4fad864bba
abdelhadia72 <thisisavnb@gmail.com>
2023-12-21 00:20:54 +0100

    testing 2

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -10,4 +10,30 @@ from os import getenv
 import pycodestyle
 import inspect
 import unittest
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+class test_Amenity(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
+
+
+class Test_PEP8(unittest.TestCase):
+    """test User"""
+    def test_pep8_user(self):
+        """test pep8 style"""
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
 
