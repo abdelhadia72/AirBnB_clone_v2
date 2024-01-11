af20a7b988a2d7ed6903f2f53c11d3043b2b52af
abdelhadia72 <thisisavnb@gmail.com>
2024-01-11 19:01:03 +0100

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,36 +10,29 @@ apt-get install -y nginx
 path="/data/web_static/releases/test/"
 mkdir -p $path && touch $path/index.html
 
+#!/bin/bash
+
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" | sudo tee /data/web_static/releases/test/index.html
+
+
 # creat a new symblic link
-ln -s /data/web_static/current /data/web_static/releases/test/
+ln -fs /data/web_static/current /data/web_static/releases/test/
 
 # change ownership
 sudo chown -R ubuntu:ubuntu /data/
 
 # update the Nginx configuration
-sudo bash -c "cat > /etc/nginx/sites-available/default" <<EOF
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+sudo sed -i '/location \/hbnb_static\//a \
+    alias /data/web_static/current/;' /etc/nginx/sites-available/default
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=fx2Z5ZD_Rbo;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}
-EOF
-
+# restart nginx
 sudo service nginx restart
