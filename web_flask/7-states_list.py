350b6aefd61e3e81f94852491b1ed7dc574c37e0
abdelhadia72 <thisisavnb@gmail.com>
2024-01-27 21:56:26 +0100

    fix taks 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,49 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello():
+    """ say hello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ show HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ show varible text """
+    return f'C {text.replace("_", " ")}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    """ show variable text """
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>')
+def check_integer(n):
+    """ Show only integers """
+    return (f"{n} is a number")
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ display number """
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def even_or_odd(n):
+    """ display number even or odd """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 def handle_teardown():
     """ close storage """
     storage.close()
