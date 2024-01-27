9663c55bf39ee401f854d0b950a35d0828bf5870
abdelhadia72 <thisisavnb@gmail.com>
2024-01-27 21:43:55 +0100

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close(error):
-    """ close storage """
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """ display page """
     status = storage.all(State).values()
     return render_template("7-states_list.html", states=status)
+
+
+@app.teardown_appcontext
+def close(self):
+    """ close storage """
+    storage.close()
 
 
 if __name__ == '__main__':
