33f4944d2c1d0d6eb72662c81c0905f535b1dcaf
abdelhadia72 <thisisavnb@gmail.com>
2024-01-27 19:46:15 +0100

    edit now (90)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -15,8 +16,9 @@ def handle_teardown(self):
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """ display page """
-    states = storage.all('State').values()
-    return render_template("7-states_list.html", states=states)
+    return render_template(
+        "7-states_list.html",
+        states=storage.all(State).values())
 
 
 if __name__ == '__main__':
