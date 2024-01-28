42a1c0a2232ced96ece4304015bee149d1014f94
abdelhadia72 <thisisavnb@gmail.com>
2024-01-28 20:12:26 +0100

    Task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-""" flast application """
+"""list the states of my database"""
+
 
 from flask import Flask, render_template
 from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-@app.route('/cities_by_states/<state_id>', strict_slashes=False)
-def cities_by_states(state_id=None):
-    """ Display states and cities """
-    if state_id:
-        state = storage.get("State", state_id)
-        if state:
-            cities = state.cities
-            return render_template(
-                '8-cities_by_states.html',
-                state=state,
-                cities=cities)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """ show cities """
 
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+    States = storage.all(State)
+
+    return render_template("8-cities_by_states.html", states=States)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Closes storage """
+def tear(e):
+    """ tear """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
